bc2791d13edff42ba17d13b57d641c6b0363e88f
PirateOwnerOfTheBoat <michal.fenik@protonmail.com>
2021-12-25 11:48:33 +0100

    Fixed success alert in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,9 @@
       <div class="w-100">
         <b-alert
             v-model="showAlert"
-            v-text="message"
             variant="success"
             dismissible
-        ></b-alert>
+        >{{ message }}</b-alert>
         <h1>Home</h1>
       </div>
     </div>
